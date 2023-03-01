bffe3258c519954aca7c9c31f5f93db95e0534b2
DireWolf707 <rahulsood707@gmail.com>
2023-03-01 15:11:17 +0530

    added project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import { Box } from "@mui/material"
+import { Home, Channel, Video, Search } from "./pages"
+import NavBar from "./components/NavBar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search/:searchTerm" element={<Search />} />
+        <Route path="/channel/:channelId" element={<Channel />} />
+        <Route path="/video/:videoId" element={<Video />} />
+      </Routes>
+    </Box>
   )
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home"
+import Search from "./Search"
+import Channel from "./Channel"
+import Video from "./Video"
+
+export { Home, Search, Channel, Video }
