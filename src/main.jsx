f2a72be40b27d5a870ad4841a7b9450f49911ec3
DireWolf707 <rahulsood707@gmail.com>
2023-03-01 22:49:28 +0530

    added custom mui theme

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,27 @@ import "./index.css"
 import { BrowserRouter as Router } from "react-router-dom"
 import { Provider } from "react-redux"
 import { store } from "./store"
+import { createTheme, ThemeProvider } from "@mui/material"
+
+const theme = createTheme({
+  palette: {
+    notSelected: {
+      main: "#fff",
+      contrastText: "#ba000d",
+    },
+    selected: {
+      main: "#ba000d",
+      contrastText: "#fff",
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </Provider>
 )
