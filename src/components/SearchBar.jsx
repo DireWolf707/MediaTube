3e0193e4f46fad99fb1d78a1fe10a990c12a29b8
DireWolf707 <rahulsood707@gmail.com>
2023-03-02 16:05:00 +0530

    added search functionality in search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,28 @@
-import React from "react"
+import React, { useRef } from "react"
 import SearchIcon from "@mui/icons-material/Search"
 import { Box, Paper, IconButton } from "@mui/material"
+import { useDispatch } from "react-redux"
+import { changeSearchTerm } from "../store"
+import { useNavigate } from "react-router-dom"
 
 const SearchBar = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const inpRef = useRef(null)
+  const searchHandler = (e) => {
+    e.preventDefault()
+    const searchTerm = inpRef.current.value
+    if (searchTerm.length == 0) return
+    dispatch(changeSearchTerm(searchTerm))
+    navigate(`/search/${searchTerm}`)
+    inpRef.current.value = ""
+  }
+
   return (
     <Paper
+      component="form"
+      onSubmit={searchHandler}
       sx={{
         display: "flex",
         // flexShrink: 1,
@@ -14,6 +32,7 @@ const SearchBar = () => {
       }}
     >
       <Box
+        ref={inpRef}
         component="input"
         sx={{
           flexGrow: 1,
@@ -23,7 +42,7 @@ const SearchBar = () => {
           color: "#ba000d",
         }}
       />
-      <IconButton sx={{ border: 3, borderColor: "#ba000d", color: "#ba000d" }}>
+      <IconButton type="submit" sx={{ border: 3, borderColor: "#ba000d", color: "#ba000d" }}>
         <SearchIcon />
       </IconButton>
     </Paper>
